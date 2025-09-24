1071fd34a64bf26d32836d203c4cc967129daa27
farrelfauzan98 <farrel.habilis@gmail.com>
2025-09-25 00:15:54 +0700

    fix: Response Interface

--- modinity-pokemon-service/src/interfaces/IResponse.ts
+++ modinity-pokemon-service/src/interfaces/IResponse.ts
@@ -1,8 +1,6 @@
 import { type AbstractEntity } from '../common/abstract.entity';
-import { type PageMetaDto } from '../common/dtos/page-meta.dto';
 
 export interface IResponse {
   statusCode: number;
   data: AbstractEntity | AbstractEntity[];
-  meta?: PageMetaDto;
 }
